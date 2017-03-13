f8df4881e1cebea6761ab3230e3204582dc09787
JesseLivezey <jesse.livezey@gmail.com>
2017-03-13 11:47:05 -0700

    replaced old file

--- gabor_fit/fit.py
+++ gabor_fit/fit.py
@@ -1,187 +1,312 @@
-#!/usr/bin/env python
-import h5py, os
+import theano
+import theano.tensor as T
+from scipy.optimize import minimize
+from scipy.ndimage.filters import gaussian_filter as gf
+from numpy import fft
+
 import numpy as np
-from functools import wraps
-
-from gabor_fit.plot import plot_gabors
 
 
-def generate_gabor(pixels, x, y, theta, stdx, stdy, lamb, phase):
-    """
-    Generate a gabor filter based on given parameters.
+def setup_graph():
+    n_x = T.lscalar('n_x')
+    n_y = T.lscalar('n_y')
+    pos_x = T.arange(n_x).dimshuffle(0, 'x', 'x')
+    pos_y = T.arange(n_y).dimshuffle('x', 0, 'x')
 
-    Parameters
-    ----------
-    pixels : tuple of ints
-        Height and width of patch.
-    x : float
-        x Location of center of gabor in pixels.
-    y : float
-        y Location of center of gabor in pixels.
-    theta : float
-        Rotation of gabor in plane in degrees.
-    stdx : float
-        Width of gaussian window along rot(x) in pixels.
-    stdy : float
-        Width of gaussian window along rot(y) in pixels.
-    lamb : float
-        Wavelength of sine funtion in pixels along rot(x).
-    phase : float
-        Phase of sine function in degrees.
+    params = T.dvector('params')
+    s_params = split_params(params)
+    x, y, theta, phi, lkx, lvx, lvy = s_params
 
-    Returns
-    -------
-    gabor : ndarray
-        2d array of pixel values.
-    """
-    x_coords = np.arange(0, pixels[0])
-    y_coords = np.arange(0, pixels[1])
-    xx, yy = np.meshgrid(x_coords, y_coords)
-    unit2rad = 2. * np.pi 
-    deg2rad = 2. * np.pi / 360.
-    xp = (xx - x) * np.cos(deg2rad * theta) - (yy - y) * np.sin(deg2rad * theta)
-    yp = (xx - x) * np.sin(deg2rad * theta) - (yy - y) * np.cos(deg2rad * theta)
-    gabor = (np.exp(-xp**2 / (2. * stdx**2) - yp**2 / (2. * stdy**2)) *
-             np.sin(unit2rad * (xp / lamb) - deg2rad * phase))
-    norm = np.sqrt((gabor**2).sum())
-    return gabor/norm
+    xp = x.dimshuffle('x', 'x', 0)
+    yp = y.dimshuffle('x', 'x', 0)
+    thetap = theta.dimshuffle('x', 'x', 0)
+    phip = phi.dimshuffle('x', 'x', 0)
+    lkxp = lkx.dimshuffle('x', 'x', 0)
+    lvxp = lvx.dimshuffle('x', 'x', 0)
+    lvyp = lvy.dimshuffle('x', 'x', 0)
 
-def _generate_fixed_loc_set(pixels, x, y, theta, stdx, stdy, lamb, phase):
-    """
-    Generate gabor filters at a fixed x, y location
-    for the parameters ranges given.
+    x_prime = T.cos(theta)*(pos_x-x) -T.sin(theta)*(pos_y-y)
+    y_prime = T.sin(theta)*(pos_x-x) +T.cos(theta)*(pos_y-y)
 
-    Parameters
-    ----------
-    pixels : tuple of ints
-        Height and width of patch.
-    x : float
-        x Location of center of gabor in pixels.
-    y : float
-        y Location of center of gabor in pixels.
-    theta : array
-        Rotations of gabor in plane in degrees.
-    stdx : array
-        Widths of gaussian window along rot(x) in pixels.
-    stdy : array
-        Widths of gaussian window along rot(y) in pixels.
-    lambda : array
-        Wavelengths of sine funtion in pixels along rot(x).
-    phase : array
-        Phases of sine function in degrees.
+    envelope = T.exp(-x_prime**2/T.exp(lvxp)-y_prime**2/T.exp(lvyp))
+    phase = T.sin(lkxp * x_prime+phip)
+    gabor = envelope * phase
 
-    Returns
-    -------
-    gabors : ndarray
-        nd array of pixel values.
-    """
-    lengths = (len(theta), len(stdx), len(stdy), len(lamb), len(phase))
-    gabors = np.zeros(lengths + pixels)
-    for ii, th in enumerate(theta):
-        for jj, sx in enumerate(stdx):
-            for kk, sy in enumerate(stdy):
-                for nn, la in enumerate(lamb):
-                    for mm, ph in enumerate(phase):
-                        gabors[ii, jj, kk, nn, mm] = generate_gabor(pixels,
-                                                                    x, y, 
-                                                                    th, sx, sy,
-                                                                    la, ph)
-    return gabors
+    gabor_norm = T.sqrt((gabor**2).sum(axis=(0, 1), keepdims=True))
+    envelope_norm = T.sqrt((envelope**2).sum(axis=(0, 1), keepdims=True))
+    phase_norm = T.sqrt((phase**2).sum(axis=(0, 1), keepdims=True))
 
-@wraps(_generate_fixed_loc_set)
-def generate_fixed_loc_set(pixels, x, y, *args):
-    # Convert ranges to values.
-    largs = tuple(np.linspace(*arg, endpoint=False) for arg in args)
-    return _generate_fixed_loc_set(pixels, x, y, *largs)
+    gabor = gabor/gabor_norm
+    envelope = envelope/envelope_norm
+    phase = phase/phase_norm
 
+    return params, s_params, n_x, n_y, gabor, envelope, phase
 
-def compare(dicts, gabors):
-    """
-    Finds best fit for dicts.
+def fit_lvx_lvy_function(data):
+    params, s_params, n_x, n_y, gabor, envelope, phase = setup_graph()
+    x, y, theta, phi, lkx, lvx, lvy = s_params
 
-    Parameters
-    ----------
-    dicts : ndarray
-        dicts, features
-    gabors : ndarray
-        Parameter dims then feature dims.
+    se = ((data-gabor)**2).sum(axis=(0, 1))
+    mse = se.mean().astype('float64')
+    grad = T.grad(mse, params, consider_constant=(x, y, theta, phi, lkx))
+    return params, mse, se, grad, gabor, n_x, n_y
 
-    Returns
-    -------
-    indxs : ints
-        Indices in params.
-    vals : floats
-        Inner products.
-    """
-    shape = gabors.shape
-    gabors = gabors.reshape(np.prod(shape[:-2]), -1)
-    # Normalize dicts
-    norms = np.linalg.norm(dicts, axis=1)[:, np.newaxis]
-    dicts = dicts/norms
+def fit_theta_phi_lkx_function(data):
+    params, s_params, n_x, n_y, gabor, envelope, phase = setup_graph()
+    x, y, theta, phi, lkx, lvx, lvy = s_params
 
-    overlaps = dicts.dot(gabors.T)
-    indxs = overlaps.argmax(axis=1)
-    vals = overlaps[range(indxs.size), indxs]
-    indxs = np.unravel_index(indxs, shape[:-2])
-    return (indxs, vals)
+    se = ((data-gabor)**2).sum(axis=(0, 1))
+    mse = se.mean().astype('float64')
+    grad = T.grad(mse, params, consider_constant=(x, y, lvx, lvy))
+    return params, mse, se, grad, gabor, n_x, n_y
 
-class GaborSet(object):
-    """
-    Fit gabor filters to a dictionary.
+def fit_theta_phi_function(data):
+    params, s_params, n_x, n_y, gabor, envelope, phase = setup_graph()
+    x, y, theta, phi, lkx, lvx, lvy = s_params
 
-    Parameters
-    ----------
-    pixels : tuple of ints
-        Height and width of patch.
-    theta : array
-        Rotations of gabor in plane in degrees.
-    stdx : array
-        Widths of gaussian window along rot(x) in pixels.
-    stdy : array
-        Widths of gaussian window along rot(y) in pixels.
-    lambda : array
-        Wavelengths of sine funtion in pixels along rot(x).
-    phase : array
-        Phases of sine function in degrees.
-    """
-    def __init__(self, pixels, theta, stdx, stdy, lamb, phase):
-        self.pixels = pixels
-        self.theta = theta
-        self.stdx = stdx
-        self.stdy = stdy
-        self.lamb = lamb
-        self.phase = phase
-    
-    def fit(self, dicts):
-    """
-    Fit gabor filters to a dictionary.
+    se = ((data-phase)**2).sum(axis=(0, 1))
+    mse = se.mean().astype('float64')
+    grad = T.grad(mse, params, consider_constant=[lkx])
+    return params, mse, se, grad, phase, n_x, n_y
 
-    Parameters
-    ----------
-    dicts : ndarray
-        batch, dim
-    """
-    n_dicts = dicts.shape[0]
-    best_fits = np.zeros(n_dicts, 7)
-    best_vals = np.zeros(n_dicts)
-    for xx in xrange(pixels[0]):
-        for yy in xrange(pixels[1]):
-            gabors = generate_fixed_loc_set(pixels, xx, yy, theta, stdx,
-                                            stdy, lamb, phase)
-            indxs, vals = compare(dicts, gabors)
-            for ii in xrange(n_dicts):
-                if vals[ii] > best_vals[ii]:
-                    best_vals[ii] == vals[ii]
-                    best_fits = self.param_values[indxs]
-    return best_fits
+def fit_only_envelope_function(data):
+    params, s_params, n_x, n_y, gabor, envelope, phase = setup_graph()
 
-def fit(dicts):
+    se = ((data-envelope)**2).sum(axis=(0, 1))
+    mse = se.mean().astype('float64')
+    grad = T.grad(mse, params)
+    return params, mse, se, grad, envelope, n_x, n_y
 
-if __name__ == '__main__':
-    gabors = generate_fixed_loc_set((16, 16), 5, 5, (0, 360, 2), (1, 5, 2), (1, 5, 2), (2, 10, 2), (0, 360, 2))
-    indxs, vals = compare(gabors.reshape(-1, 256), gabors)
-    print indxs.shape
-    for ii in range(5):
-        print indxs[ii], indxs[ii].shape
-    gabors = gabors.reshape(-1, 256)
-    plot_gabors(gabors)
+def fit_x_y_function(data):
+    params, s_params, n_x, n_y, gabor, envelope, phase = setup_graph()
+    x, y, theta, phi, lkx, lvx, lvy = s_params
+
+    se = ((data-envelope)**2).sum(axis=(0, 1))
+    mse = se.mean().astype('float64')
+    grad = T.grad(mse, params, consider_constant=[theta, lvx, lvy])
+    return params, mse, se, grad, gabor, n_x, n_y
+
+def fit_phi_x_y_function(data):
+    params, s_params, n_x, n_y, gabor, envelope, phase = setup_graph()
+    x, y, theta, phi, lkx, lvx, lvy = s_params
+
+    se = ((data-gabor)**2).sum(axis=(0, 1))
+    mse = se.mean().astype('float64')
+    grad = T.grad(mse, params, consider_constant=[theta, lkx, lvx, lvy])
+    return params, mse, se, grad, gabor, n_x, n_y
+
+def fit_envelope_function(data):
+    params, s_params, n_x, n_y, gabor, envelope, phase = setup_graph()
+    x, y, theta, phi, lkx, lvx, lvy = s_params
+
+    se = ((data-gabor)**2).sum(axis=(0, 1))
+    mse = se.mean().astype('float64')
+    grad = T.grad(mse, params, consider_constant=[theta, lkx])
+    return params, mse, se, grad, gabor, n_x, n_y
+
+def fit_all_function(data):
+    params, s_params, n_x, n_y, gabor, envelope, phase = setup_graph()
+
+    se = ((data-gabor)**2).sum(axis=(0, 1))
+    mse = se.mean().astype('float64')
+    grad = T.grad(mse, params)
+    return params, mse, se, grad, gabor, n_x, n_y
+
+def split_params(params):
+    n_samples = params.shape[0]//7
+    x = params[:n_samples].astype('float32')
+    y = params[n_samples:2*n_samples].astype('float32')
+    theta = params[2*n_samples:3*n_samples].astype('float32')
+    phi = params[3*n_samples:4*n_samples].astype('float32')
+    lkx = params[4*n_samples:5*n_samples].astype('float32')
+    lvx = params[5*n_samples:6*n_samples].astype('float32')
+    lvy = params[6*n_samples:].astype('float32')
+    return x, y, theta, phi, lkx, lvx, lvy
+
+class GaborFit(object):
+    def __init__(self):
+        self.data = theano.shared(np.empty((1,1,1), dtype='float32'))
+
+        """
+        (params, mse, se, grad, gabor,
+         n_x_s, n_y_s)  = fit_only_envelope_function(self.data)
+        self._fit_only_envelope = theano.function([params], [mse, grad],
+                                                  givens={n_x_s: self.data.shape[0],
+                                                          n_y_s: self.data.shape[1]})
+        self._fit_only_envelope_se = theano.function([params], se,
+                                                     givens={n_x_s: self.data.shape[0],
+                                                             n_y_s: self.data.shape[1]})
+
+        (params, mse, se, grad, gabor,
+         n_x_s, n_y_s)  = fit_envelope_function(self.data)
+        self._fit_envelope = theano.function([params], [mse, grad],
+                                             givens={n_x_s: self.data.shape[0],
+                                                     n_y_s: self.data.shape[1]})
+        self._fit_envelope_se = theano.function([params], se,
+                                                givens={n_x_s: self.data.shape[0],
+                                                        n_y_s: self.data.shape[1]})
+        """
+
+        (params, mse, se, grad, gabor,
+         n_x_s, n_y_s)  = fit_x_y_function(self.data)
+        self._fit_x_y = theano.function([params], [mse, grad],
+                                        givens={n_x_s: self.data.shape[0],
+                                                n_y_s: self.data.shape[1]})
+        self._fit_x_y_se = theano.function([params], se,
+                                           givens={n_x_s: self.data.shape[0],
+                                                   n_y_s: self.data.shape[1]})
+
+        (params, mse, se, grad, gabor,
+         n_x_s, n_y_s)  = fit_phi_x_y_function(self.data)
+        self._fit_phi_x_y = theano.function([params], [mse, grad],
+                                            givens={n_x_s: self.data.shape[0],
+                                                    n_y_s: self.data.shape[1]})
+        self._fit_phi_x_y_se = theano.function([params], se,
+                                               givens={n_x_s: self.data.shape[0],
+                                                       n_y_s: self.data.shape[1]})
+
+        (params, mse, se, grad, gabor,
+         n_x_s, n_y_s)  = fit_theta_phi_lkx_function(self.data)
+        self._fit_theta_phi_lkx = theano.function([params], [mse, grad],
+                                                  givens={n_x_s: self.data.shape[0],
+                                                          n_y_s: self.data.shape[1]})
+
+        self._fit_theta_phi_lkx_se = theano.function([params], se,
+                                                     givens={n_x_s: self.data.shape[0],
+                                                             n_y_s: self.data.shape[1]})
+
+        """
+        (params, mse, se, grad, gabor,
+         n_x_s, n_y_s)  = fit_theta_phi_function(self.data)
+        self._fit_theta_phi = theano.function([params], [mse, grad],
+                                              givens={n_x_s: self.data.shape[0],
+                                                      n_y_s: self.data.shape[1]})
+        self._fit_theta_phi_se = theano.function([params], se,
+                                                 givens={n_x_s: self.data.shape[0],
+                                                         n_y_s: self.data.shape[1]})
+        """
+
+        (params, mse, se, grad, gabor,
+         n_x_s, n_y_s)  = fit_all_function(self.data)
+        self._fit_all = theano.function([params], [mse, grad],
+                                        givens={n_x_s: self.data.shape[0],
+                                                n_y_s: self.data.shape[1]})
+        self._fit_all_se = theano.function([params], se,
+                                           givens={n_x_s: self.data.shape[0],
+                                                   n_y_s: self.data.shape[1]})
+
+        (params, mse, se, grad, gabor,
+         n_x_s, n_y_s)  = fit_lvx_lvy_function(self.data)
+        self._fit_lvx_lvy = theano.function([params], [mse, grad],
+                                            givens={n_x_s: self.data.shape[0],
+                                                    n_y_s: self.data.shape[1]})
+        self._fit_lvx_lvy_se = theano.function([params], se,
+                                               givens={n_x_s: self.data.shape[0],
+                                                       n_y_s: self.data.shape[1]})
+
+        params_s, s_params, n_x_s, n_y_s, gabor, envelope, phase = setup_graph()
+        self._make_gabor = theano.function([params_s, n_x_s, n_y_s], gabor)
+        self._make_phase = theano.function([params_s, n_x_s, n_y_s], phase)
+        self._make_envelope = theano.function([params_s, n_x_s, n_y_s], envelope)
+
+    def fit(self, X, var_init=.05):
+        # Calculate different versions of the data
+        n_x, n_y, n_samples = X.shape
+        init = np.zeros(7*n_samples)
+
+        X_norm = np.sqrt((X**2).sum(axis=(0, 1), keepdims=True))
+        X = X/X_norm
+
+        fao = np.array([gf(abs(xi), 2, mode='constant', cval=0.)
+                        for xi in X.transpose(2, 0, 1)]).transpose(1, 2, 0).astype('float32')
+
+        fao_norm = np.sqrt((fao**2).sum(axis=(0, 1), keepdims=True))
+        fao = fao/fao_norm
+
+        aps = abs(fft.fft2(X, axes=(0, 1)))
+        aps_norm = np.sqrt((aps**2).sum(axis=(0, 1), keepdims=True))
+        aps = aps/aps_norm
+        freqs = fft.fftfreq(n_x)[:, np.newaxis] + 1j*fft.fftfreq(n_y)[np.newaxis, :]
+
+        thetas = np.linspace(0., np.pi, 8)
+        k_min = 2.*np.pi*min(1./n_x, 1./n_y)
+        k_max = 2.*np.pi*.75
+        ks = np.linspace(k_min, k_max, 20, endpoint=True)
+
+        def choose_best(best_se, best_params, se, params):
+            compare = se < best_se
+            best_params = best_params.reshape(7, -1)
+            params = params.reshape(7, -1)
+            best_se[compare] = se[compare]
+            best_params[:, compare] = params[:, compare]
+            return best_se, best_params.ravel()
+
+        best_se = np.inf*np.ones(n_samples)
+        best_params = np.zeros(7*n_samples)
+
+        x = []
+        for vi in [var_init/2., var_init, 2.*var_init]:
+            init = np.zeros(7*n_samples)
+            init[:n_samples] = n_x/2.
+            init[n_samples:2*n_samples] = n_y/2.
+            init[4*n_samples:5*n_samples] = k_min
+            init[5*n_samples:6*n_samples] = np.log(vi*(n_x)**2)
+            init[6*n_samples:7*n_samples] = np.log(vi*(n_y)**2)
+
+            # Fit envelope mean
+            func = self._fit_x_y
+            self.data.set_value(fao.astype('float32'))
+            res = minimize(func, init, method='L-BFGS-B', jac=True)
+            x.append(res.x)
+            params = res.x
+            """
+            se = self._fit_x_y_se(params)
+            best_se, best_params = choose_best(best_se, best_params, se, params)
+            """
+
+            self.data.set_value(X.astype('float32'))
+
+            x.append(best_params)
+
+            func = self._fit_theta_phi_lkx
+            func_se = self._fit_theta_phi_lkx_se
+            for theta in thetas:
+                for k in ks:
+                    init = params.copy()
+                    init[2*n_samples:3*n_samples] = theta
+                    init[4*n_samples:5*n_samples] = k
+                    res = minimize(func, init, method='L-BFGS-B', jac=True)
+                    params = res.x
+                    se = func_se(params)
+                    best_se, best_params = choose_best(best_se, best_params, se, params)
+                    x.append(best_params)
+                #print theta, k, se.mean(), best_se.mean()
+
+        # Fit envelope widths
+        func = self._fit_lvx_lvy
+        res = minimize(func, best_params, method='L-BFGS-B', jac=True)
+        params = res.x
+        se = self._fit_lvx_lvy_se(params)
+        best_se, best_params = choose_best(best_se, best_params, se, params)
+
+        x.append(best_params)
+
+        # Fit envelope center and phase
+        func = self._fit_phi_x_y
+        res = minimize(func, best_params, method='L-BFGS-B', jac=True)
+        params = res.x
+        se = self._fit_phi_x_y_se(params)
+        best_se, best_params = choose_best(best_se, best_params, se, params)
+
+        x.append(best_params)
+
+        return x, split_params(best_params), best_se
+
+    def make_gabor(self, params, n_x, n_y):
+        return self._make_gabor(params, n_x, n_y)
+
+    def make_phase(self, params, n_x, n_y):
+        return self._make_phase(params, n_x, n_y)
+
+    def make_envelope(self, params, n_x, n_y):
+        return self._make_envelope(params, n_x, n_y)
